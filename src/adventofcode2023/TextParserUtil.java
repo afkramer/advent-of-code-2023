61bf59903243f11b4b7f5c8a1944c1a8eac45e72
Kramer, Adriana (SE-A/35) <adri.kramer@gmail.com>
2023-12-01 08:36:09 +0100

    Solve part one day 1

---
+++ src/adventofcode2023/TextParserUtil.java
@@ -0,0 +1,19 @@
+package adventofcode2023;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class TextParserUtil {
+    public static List<String> readData(String fileName) {
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get("./res/" + fileName))) {
+            return reader.lines()
+                         .toList();
+        } catch (IOException e) {
+            System.err.print("Could not read data.");
+            return null;
+        }
+    }
+}
--- src/adventofcode2023/day01/Day01.java
+++ src/adventofcode2023/day01/Day01.java
@@ -1,16 +1,56 @@
 package adventofcode2023.day01;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import adventofcode2023.Day;
+import adventofcode2023.TextParserUtil;
 
 public class Day01 implements Day {
 
+    private static String[] numbersAsStrings = { "one", "two", "three", "four", "five", "six", "seven", "eight",
+            "nine" };
+    private static List<String> inputs = TextParserUtil.readData("day01.txt");
+
     @Override
     public void partOne() {
+        int sum = 0;
 
+        for (String input : inputs) {
+            String[] characters = input.split("");
+            List<Integer> ints = new ArrayList<Integer>();
+            Arrays.stream(characters)
+                  .forEach(c -> {
+                      if (parseInteger(c) != null) {
+                          ints.add(parseInteger(c));
+                      }
+                  });
+            Integer firstAndLast = parseInteger("" + ints.get(0) + ints.get(ints.size() - 1));
+            sum += firstAndLast;
+        }
+        System.out.println(sum);
     }
 
     @Override
     public void partTwo() {
+        int sum = 0;
+        inputs = 
+        
+        for (String input : inputs) {
+            
+        }
+    }
+
+    public Integer parseInteger(String string) {
+        try {
+            return Integer.parseInt(string);
+        } catch (NumberFormatException nfe) {
+            return null;
+        }
+    }
+
+    public String replaceNumberWordsWithNumbers(String string) {
 
     }
 
